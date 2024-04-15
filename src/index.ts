0426bb740b3dfa69c6392ef650e269c788db4c11
alexzhang1030 <1642114555@qq.com>
2024-04-15 15:06:52 +0800

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { pathToTree } from './fn'
 export { PathTreeBuilder } from './builder'
-export { ENTRY_NAME, DEFAULT_SEP } from './constants'
+export { ENTRY_NAME, DEFAULT_SEP, ROOT_NAME } from './constants'
 export { getFileNameAndExt, walkPathTree } from './utils'
 
 export type { NodeItem, ParseResults, TreeNode } from './types'
