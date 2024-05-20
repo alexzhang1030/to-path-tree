7ce717a102fe5a80df79332f7a28c4faa6e6ec94
Alex <alexzhang1030@foxmail.com>
2024-05-20 12:06:34 +0800

    chore: update

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,8 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu({
+  rules: {
+    'regexp/no-super-linear-backtracking': 'off',
+    'regexp/no-misleading-capturing-group': 'off',
+  },
+})
